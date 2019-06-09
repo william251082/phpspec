165bbc6bd2ddac319867429026234e7300b5bcbc
William del Rosario <williamdelrosario@yahoo.com>
2019-06-09 11:39:42 +0200

    Matchers, examples and Generations

--- src/Entity/Dinosaur.php
+++ src/Entity/Dinosaur.php
@@ -4,4 +4,15 @@ namespace App\Entity;
 
 class Dinosaur
 {
+    private  $length = 0;
+
+    public function getLength()
+    {
+        return $this->length;
+    }
+
+    public function setLength(int $length)
+    {
+        return $this->length = $length;
+    }
 }
